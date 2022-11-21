982134202a79d03b879f677638cef97c326c415b
Atharva Deore <www.atharvadeore22@gmail.com>
2022-11-21 10:26:48 +0530

    changes

--- twilio.js
+++ twilio.js
@@ -12,13 +12,13 @@ app.post('/sms', (req, res) => {
 });
 
 app.get('/send-sms', (req, res) => {
-    const accountSid = 'AC7031bd2a90dd96b1ae4ad5541c0010a5'//'AC5cdde1fa27b1bc6c625b261a82e42abf'//process.env.TWILIO_ACCOUNT_SID||'AC7031bd2a90dd96b1ae4ad5541c0010a5';
-    const authToken = '072f793418d670fbb934b8110c943431'//'c671709e4b24272d5b6b93adbc2efabe'//process.env.TWILIO_AUTH_TOKEN||'072f793418d670fbb934b8110c943431';
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = require('twilio')(accountSid, authToken);
     client.messages
       .create({
          body: 'Hello there!',
-         from: '+13862515314',//'+14254752143',//'+13862515314',
+         from: process.env.FROM,
          to: '+919307387132'
        })
       .then(message => console.log(message.sid));
